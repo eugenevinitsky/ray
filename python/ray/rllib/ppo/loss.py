326323f71cbd42e39235a89f9857d5086ce48656
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-03-20 15:30:56 -0700

    reverted loss to old loss

--- python/ray/rllib/ppo/loss.py
+++ python/ray/rllib/ppo/loss.py
@@ -48,28 +48,41 @@ class ProximalPolicyLoss(object):
         self.entropy = self.curr_dist.entropy()
 
         # Lists allow us to handle multiagents and single agents similarly
-        if not isinstance(curr_logp, list):
-            curr_logp = [curr_logp]
-            prev_logp = [self.prev_dist.logp(actions)]
-            self.kl = [self.prev_dist.kl(self.curr_dist)]
-            self.entropy = [self.curr_dist.entropy()]
+        # if not isinstance(curr_logp, list):
+        #     curr_logp = [curr_logp]
+        #     prev_logp = [self.prev_dist.logp(actions)]
+        #     self.kl = [self.prev_dist.kl(self.curr_dist)]
+        #     self.entropy = [self.curr_dist.entropy()]
+        #
+        # self.ratio = np.asarray([tf.exp(a - b) for a, b
+        #                          in zip(curr_logp, prev_logp)])
+        # self.mean_kl = [tf.reduce_mean(kl_i) for kl_i in self.kl]
+        # self.mean_entropy = [tf.reduce_mean(entropy_i)
+        #                      for entropy_i in self.entropy]
+        # self.surr1 = [ratio_i * advantages for ratio_i in self.ratio]
+        # self.surr2 = [advantages *
+        #               tf.clip_by_value(ratio_i,
+        #                                1 - config["clip_param"],
+        #                                1 + config["clip_param"])
+        #               for ratio_i in self.ratio]
+        # self.surr = [tf.minimum(surr1_i, surr2_i) for surr1_i, surr2_i
+        #              in zip(self.surr1, self.surr2)]
+        # self.mean_policy_loss = tf.reduce_mean(-tf.add_n(self.surr))
+        # self.surr = tf.add_n(self.surr)
+        # self.entropy = tf.add_n(self.entropy)
+        # kl_prod = kl_coeff*self.kl
 
-        self.ratio = np.asarray([tf.exp(a - b) for a, b
-                                 in zip(curr_logp, prev_logp)])
-        self.mean_kl = [tf.reduce_mean(kl_i) for kl_i in self.kl]
-        self.mean_entropy = [tf.reduce_mean(entropy_i)
-                             for entropy_i in self.entropy]
-        self.surr1 = [ratio_i * advantages for ratio_i in self.ratio]
-        self.surr2 = [advantages *
-                      tf.clip_by_value(ratio_i,
-                                       1 - config["clip_param"],
-                                       1 + config["clip_param"])
-                      for ratio_i in self.ratio]
-        self.surr = [tf.minimum(surr1_i, surr2_i) for surr1_i, surr2_i
-                     in zip(self.surr1, self.surr2)]
-        self.mean_policy_loss = tf.reduce_mean(-tf.add_n(self.surr))
-        self.surr = tf.add_n(self.surr)
-        self.entropy = tf.add_n(self.entropy)
+        self.ratio = tf.exp(self.curr_dist.logp(actions) -
+                            self.prev_dist.logp(actions))
+        self.kl = self.prev_dist.kl(self.curr_dist)
+        self.mean_kl = tf.reduce_mean(self.kl)
+        self.entropy = self.curr_dist.entropy()
+        self.mean_entropy = tf.reduce_mean(self.entropy)
+        self.surr1 = self.ratio * advantages
+        self.surr2 = tf.clip_by_value(self.ratio, 1 - config["clip_param"],
+                                      1 + config["clip_param"]) * advantages
+        self.surr = tf.minimum(self.surr1, self.surr2)
+        self.mean_policy_loss = tf.reduce_mean(-self.surr)
         kl_prod = kl_coeff*self.kl
 
         if config["use_gae"]:
