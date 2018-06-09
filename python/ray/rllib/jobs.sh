862e1d5f9e514db34e6b62970b83809dbbaeba05
leflemluc <leflemluc@gmail.com>
2018-06-09 12:48:52 -0700

    new jobs

--- python/ray/rllib/jobs.sh
+++ python/ray/rllib/jobs.sh
@@ -8,11 +8,7 @@ python train.py -f tuned_examples/mujoco_tests/Swimmer-ADB.yaml
 python train.py -f tuned_examples/mujoco_tests/Walker2d-ADB.yaml
 python train.py -f tuned_examples/mujoco_tests/HalfCheetah-ADB.yaml
 python train.py -f tuned_examples/mujoco_tests/InvertedPendulum-ADB.yaml
-#python train.py -f tuned_examples/mujoco_tests/Ant.yaml
-#python train.py -f tuned_examples/mujoco_tests/HalfCheetah.yaml
-#python train.py -f tuned_examples/mujoco_tests/InvertedDoublePendulum-ADB.yaml
-#python train.py -f tuned_examples/mujoco_tests/Reacher.yaml
-#python train.py -f tuned_examples/mujoco_tests/Reacher-ADB.yaml
-#python train.py -f tuned_examples/mujoco_tests/Hopper-ADB.yaml
+python train.py -f tuned_examples/mujoco_tests/InvertedDoublePendulum-ADB.yaml
+
 
 
