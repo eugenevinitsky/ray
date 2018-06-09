5134f72078c14c4d8308bfc0397758306d0daf2d
leflemluc <leflemluc@gmail.com>
2018-06-08 21:18:47 -0700

    PPO ADB

--- python/ray/rllib/utils/process_rollout.py
+++ python/ray/rllib/utils/process_rollout.py
@@ -11,37 +11,42 @@ def discount(x, gamma):
     return scipy.signal.lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1]
 
 
-def process_rollout(rollout, reward_filter, gamma, lambda_=1.0, use_gae=True):
+def process_rollout(rollout, reward_filter, gamma, lambda_=1.0, use_gae=True, ADB=ADB):
     """Given a rollout, compute its value targets and the advantage.
-
     Args:
         rollout (PartialRollout): Partial Rollout Object
         reward_filter (Filter): Filter for processing advantanges
         gamma (float): Parameter for GAE
         lambda_ (float): Parameter for GAE
         use_gae (bool): Using Generalized Advantage Estamation
-
     Returns:
         SampleBatch (SampleBatch): Object with experience from rollout and
             processed rewards."""
 
     traj = {}
-    trajsize = len(rollout["actions"])
-    for key in rollout:
-        traj[key] = np.stack(rollout[key])
+    trajsize = len(rollout.data["actions"])
+    for key in rollout.data:
+        traj[key] = np.stack(rollout.data[key])
 
     if use_gae:
-        assert "vf_preds" in rollout, "Values not found!"
-        vpred_t = np.stack(rollout["vf_preds"] +
-                           [np.array(rollout.last_r)]).squeeze()
-        delta_t = traj["rewards"] + gamma * vpred_t[1:] - vpred_t[:-1]
-        # This formula for the advantage comes
-        # "Generalized Advantage Estimation": https://arxiv.org/abs/1506.02438
-        traj["advantages"] = discount(delta_t, gamma * lambda_)
+        if ADB:
+            Q_function = np.transpose(np.array(rollout.Q_function))
+            Q_pred_t = np.vstack((Q_function, Q_function[-1]))
+            delta_t_multi = traj["rewards"].reshape(-1, 1) + gamma * Q_pred_t[1:] - Q_pred_t[:-1]
+            traj["advantages"] = discount(delta_t_multi, gamma * lambda_)
+        else:
+            vpred_t = np.stack(rollout.data["vf_preds"] + [np.array(rollout.last_r)]).squeeze()
+            delta_t = traj["rewards"] + gamma * vpred_t[1:] - vpred_t[:-1]
+            traj["advantages"] = discount(delta_t, gamma * lambda_)
+            #traj["advantages"] = returns - vpred_t[:-1]
+
         traj["value_targets"] = traj["advantages"] + traj["vf_preds"]
+        #traj["value_targets"] = traj["rewards"] + gamma * traj["vf_preds"]
+        #traj["value_targets"] = returns
+
     else:
-        rewards_plus_v = np.stack(rollout["rewards"] +
-                                  [np.array(rollout.last_r)]).squeeze()
+        rewards_plus_v = np.stack(
+            rollout.data["rewards"] + [np.array(rollout.last_r)]).squeeze()
         traj["advantages"] = discount(rewards_plus_v, gamma)[:-1]
 
     for i in range(traj["advantages"].shape[0]):
