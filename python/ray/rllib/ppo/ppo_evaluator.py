779580f1584d4e76f5c04b809c407094e4a4658c
leflemluc <leflemluc@gmail.com>
2018-07-12 13:16:56 -0700

    clean branch with ad working

--- python/ray/rllib/ppo/ppo_evaluator.py
+++ python/ray/rllib/ppo/ppo_evaluator.py
@@ -17,7 +17,7 @@ from ray.rllib.models import ModelCatalog
 from ray.rllib.utils.sampler import SyncSampler
 from ray.rllib.utils.filter import get_filter, MeanStdFilter
 from ray.rllib.utils.process_rollout import process_rollout
-from ray.rllib.ppo.loss import ProximalPolicyLoss
+from ray.rllib.ppo.loss import ProximalPolicyGraph
 
 
 # TODO(rliaw): Move this onto LocalMultiGPUOptimizer
@@ -30,7 +30,8 @@ class PPOEvaluator(PolicyEvaluator):
     network weights. When run as a remote agent, only this graph is used.
     """
 
-    def __init__(self, registry, env_creator, config, logdir, is_remote):
+    def __init__(self, registry, env_creator, config, logdir, is_remote, ADB):
+        self.ADB = ADB
         self.registry = registry
         self.is_remote = is_remote
         if is_remote:
@@ -55,33 +56,34 @@ class PPOEvaluator(PolicyEvaluator):
 
         # Defines the training inputs:
         # The coefficient of the KL penalty.
-        shared_model = (self.config["model"].get("custom_options", {}).
-                        get("multiagent_shared_model", False))
-        if shared_model:
-            num_kl_terms = 1
-        else:
-            num_kl_terms = len(self.config["model"].get("custom_options", {}).
-                               get("multiagent_obs_shapes", [1]))
         self.kl_coeff = tf.placeholder(
-            name="newkl", shape=(num_kl_terms,), dtype=tf.float32)
+            name="newkl", shape=(), dtype=tf.float32)
 
         # The input observations.
         self.observations = tf.placeholder(
             tf.float32, shape=(None,) + self.env.observation_space.shape)
         # Targets of the value function.
         self.value_targets = tf.placeholder(tf.float32, shape=(None,))
-        # Advantage values in the policy gradient estimator.
-        self.advantages = tf.placeholder(tf.float32, shape=(None,))
 
         action_space = self.env.action_space
         self.actions = ModelCatalog.get_action_placeholder(action_space)
         self.distribution_class, self.logit_dim = ModelCatalog.get_action_dist(
-            action_space)
+            action_space, dist_type=config["dist_type"])
         # Log probabilities from the policy before the policy update.
         self.prev_logits = tf.placeholder(
             tf.float32, shape=(None, self.logit_dim))
         # Value function predictions before the policy update.
         self.prev_vf_preds = tf.placeholder(tf.float32, shape=(None,))
+
+        # Advantage values in the policy gradient estimator.
+        if self.ADB:
+            try:
+                action_dim = self.actions.shape[1]
+                self.advantages = tf.placeholder(tf.float32, shape=(None, action_dim))
+            except:
+                print("Action dim not defined")
+        else:
+            self.advantages = tf.placeholder(tf.float32, shape=(None,))
 
         if is_remote:
             self.batch_size = config["rollout_batchsize"]
@@ -93,14 +95,15 @@ class PPOEvaluator(PolicyEvaluator):
             self.per_device_batch_size = int(self.batch_size / len(devices))
 
         def build_loss(obs, vtargets, advs, acts, plog, pvf_preds):
-            return ProximalPolicyLoss(
+            return ProximalPolicyGraph(
                 self.env.observation_space, self.env.action_space,
                 obs, vtargets, advs, acts, plog, pvf_preds, self.logit_dim,
                 self.kl_coeff, self.distribution_class, self.config,
-                self.sess, self.registry)
+                self.sess, self.registry, ADB=self.ADB)
 
         self.par_opt = LocalSyncParallelOptimizer(
-            tf.train.AdamOptimizer(self.config["sgd_stepsize"]),
+            tf.train.AdamOptimizer(self.config["sgd_stepsize_policy"]),
+            tf.train.AdamOptimizer(self.config["sgd_stepsize_vf"]),
             self.devices,
             [self.observations, self.value_targets, self.advantages,
              self.actions, self.prev_logits, self.prev_vf_preds],
@@ -110,27 +113,26 @@ class PPOEvaluator(PolicyEvaluator):
 
         # Metric ops
         with tf.name_scope("test_outputs"):
-            policies = self.par_opt.get_device_losses()
-            self.mean_loss = tf.reduce_mean(
-                tf.stack(values=[
-                    policy.loss for policy in policies]), 0)
+            policies_loss, policies_baselines = self.par_opt.get_device_losses()
             self.mean_policy_loss = tf.reduce_mean(
                 tf.stack(values=[
-                    policy.mean_policy_loss for policy in policies]), 0)
-            self.mean_vf_loss = tf.reduce_mean(
-                tf.stack(values=[
-                    policy.mean_vf_loss for policy in policies]), 0)
+                    policy.loss for policy in policies_loss]), 0)
             self.mean_kl = tf.reduce_mean(
                 tf.stack(values=[
-                    policy.mean_kl for policy in policies]), 0)
+                    policy.mean_kl for policy in policies_loss]), 0)
             self.mean_entropy = tf.reduce_mean(
                 tf.stack(values=[
-                    policy.mean_entropy for policy in policies]), 0)
+                    policy.mean_entropy for policy in policies_loss]), 0)
+            self.mean_vf_loss = tf.reduce_mean(
+                tf.stack(values=[
+                    policy.mean_vf_loss for policy in policies_baselines]), 0)
 
         # References to the model weights
         self.common_policy = self.par_opt.get_common_loss()
-        self.variables = ray.experimental.TensorFlowVariables(
+        self.variables_policy = ray.experimental.TensorFlowVariables(
             self.common_policy.loss, self.sess)
+        self.variables_vf = ray.experimental.TensorFlowVariables(
+            self.common_policy.loss_vf, self.sess)
         self.obs_filter = get_filter(
             config["observation_filter"], self.env.observation_space.shape)
         self.rew_filter = MeanStdFilter((), clip=5.0)
@@ -138,7 +140,7 @@ class PPOEvaluator(PolicyEvaluator):
                         "rew_filter": self.rew_filter}
         self.sampler = SyncSampler(
             self.env, self.common_policy, self.obs_filter,
-            self.config["horizon"], self.config["horizon"])
+            self.config["horizon"], self.config["horizon"], ADB=self.ADB)
         self.sess.run(tf.global_variables_initializer())
 
     def load_data(self, trajectories, full_trace):
@@ -154,15 +156,26 @@ class PPOEvaluator(PolicyEvaluator):
              trajectories["vf_preds"] if use_gae else dummy],
             full_trace=full_trace)
 
+
     def run_sgd_minibatch(
             self, batch_index, kl_coeff, full_trace, file_writer):
         return self.par_opt.optimize(
             self.sess,
             batch_index,
+            baseline = False,
             extra_ops=[
-                self.mean_loss, self.mean_policy_loss, self.mean_vf_loss,
+                self.mean_policy_loss,
                 self.mean_kl, self.mean_entropy],
             extra_feed_dict={self.kl_coeff: kl_coeff},
+            file_writer=file_writer if full_trace else None)
+
+    def run_sgd_minibatch_baseline(
+            self, batch_index, full_trace, file_writer):
+        return self.par_opt.optimize(
+            self.sess,
+            batch_index,
+            baseline=True,
+            extra_ops=[self.mean_vf_loss],
             file_writer=file_writer if full_trace else None)
 
     def compute_gradients(self, samples):
@@ -179,11 +192,18 @@ class PPOEvaluator(PolicyEvaluator):
         objs = pickle.loads(objs)
         self.sync_filters(objs["filters"])
 
-    def get_weights(self):
-        return self.variables.get_weights()
+    def get_weights_policy(self):
+        return self.variables_policy.get_weights()
 
-    def set_weights(self, weights):
-        self.variables.set_weights(weights)
+    def set_weights_policy(self, weights):
+        self.variables_policy.set_weights(weights)
+
+    def get_weights_vf(self):
+        return self.variables_vf.get_weights()
+
+    def set_weights_vf(self, weights):
+        self.variables_vf.set_weights(weights)
+
 
     def sample(self):
         """Returns experience samples from this Evaluator. Observation
@@ -197,9 +217,10 @@ class PPOEvaluator(PolicyEvaluator):
 
         while num_steps_so_far < self.config["min_steps_per_task"]:
             rollout = self.sampler.get_data()
+            last_r = 0.0  # note: not needed since we don't truncate rollouts
             samples = process_rollout(
-                rollout, self.rew_filter, self.config["gamma"],
-                self.config["lambda"], use_gae=self.config["use_gae"])
+                rollout, last_r, self.config["gamma"],
+                self.config["lambda"], use_gae=self.config["use_gae"], ADB=self.ADB)
             num_steps_so_far += samples.count
             all_samples.append(samples)
         return SampleBatch.concat_samples(all_samples)
