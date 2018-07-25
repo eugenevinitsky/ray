82833da58414ba88df150cc73f885ccc4e3a0a01
leflemluc <leflemluc@gmail.com>
2018-07-25 16:34:33 -0700

    fix

--- python/ray/rllib/ppo/loss.py
+++ python/ray/rllib/ppo/loss.py
@@ -57,7 +57,9 @@ class ProximalPolicyGraph(object):
             self.surr1 = self.ratio_coordinate_wise * advantages
             action_dim = action_space.shape[0]
 
-            ratio_non_derivable = tf.stop_gradient(self.ratio_total)
+            ratio_coordinate_wise = tf.clip_by_value(self.ratio_coordinate_wise, 1 - config["clip_param"],
+                                          1 + config["clip_param"])
+            ratio_non_derivable = tf.reduce_prod(ratio_coordinate_wise, reduction_indices=[1])
 
             condition_1 = tf.less(ratio_non_derivable, 1 - config["clip_param"])
             condition_2 = tf.greater(ratio_non_derivable, 1 + config["clip_param"])
@@ -66,7 +68,7 @@ class ProximalPolicyGraph(object):
             alpha_vector_2 = tf.where(condition_2, tf.div(1 + config["clip_param"], alpha_vector_1 + 1e-4), alpha_vector_1)
             self.alpha_vector = tf.stop_gradient(tf.stack([alpha_vector_2**(1/action_dim) for _ in range(action_dim)], axis=1))
 
-            self.surr2 = self.ratio_coordinate_wise * self.alpha_vector * advantages
+            self.surr2 = ratio_coordinate_wise * self.alpha_vector * advantages
             self.surr = tf.reduce_sum(tf.minimum(self.surr1, self.surr2), reduction_indices=[1])
 
         else:
