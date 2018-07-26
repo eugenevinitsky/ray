4f8eb29d7c41cdf0f57951f06526901744cba212
leflemluc <leflemluc@gmail.com>
2018-07-26 14:12:12 -0700

    last version

--- python/ray/rllib/ppo/loss.py
+++ python/ray/rllib/ppo/loss.py
@@ -46,8 +46,7 @@ class ProximalPolicyGraph(object):
         # Make loss functions.
         self.ratio_coordinate_wise = tf.div(self.curr_dist.r_matrix(actions),
                             self.prev_dist.r_matrix(actions))
-        self.ratio_total = tf.exp(self.curr_dist.logp(actions) -
-                                self.prev_dist.logp(actions))
+        self.ratio_total = tf.exp(self.curr_dist.logp(actions) - self.prev_dist.logp(actions))
         self.kl = self.prev_dist.kl(self.curr_dist)
         self.mean_kl = tf.reduce_mean(self.kl)
         self.entropy = self.curr_dist.entropy()
@@ -57,18 +56,17 @@ class ProximalPolicyGraph(object):
             self.surr1 = self.ratio_coordinate_wise * advantages
             action_dim = action_space.shape[0]
 
-            ratio_coordinate_wise = tf.clip_by_value(self.ratio_coordinate_wise, 1 - config["clip_param"],
-                                          1 + config["clip_param"])
-            ratio_non_derivable = tf.reduce_prod(ratio_coordinate_wise, reduction_indices=[1])
+            condition_1 = tf.less(self.ratio_total , 1 - config["clip_param"])
+            condition_2 = tf.greater(self.ratio_total , 1 + config["clip_param"])
 
-            condition_1 = tf.less(ratio_non_derivable, 1 - config["clip_param"])
-            condition_2 = tf.greater(ratio_non_derivable, 1 + config["clip_param"])
+            one_vector = tf.ones_like(self.ratio_total)
 
-            alpha_vector_1 = tf.where(condition_1, tf.div(1 - config["clip_param"], ratio_non_derivable + 1e-4), ratio_non_derivable)
-            alpha_vector_2 = tf.where(condition_2, tf.div(1 + config["clip_param"], alpha_vector_1 + 1e-4), alpha_vector_1)
-            self.alpha_vector = tf.stop_gradient(tf.stack([alpha_vector_2**(1/action_dim) for _ in range(action_dim)], axis=1))
+            alpha_vector_1 = tf.where(condition_1, tf.div(1 - config["clip_param"], self.ratio_total), one_vector)
+            alpha_vector_2 = tf.where(condition_2, tf.div(1 + config["clip_param"], self.ratio_total), one_vector)
+            alpha_vector = alpha_vector_1 * alpha_vector_2
+            self.alpha_vector = tf.stop_gradient(tf.stack([alpha_vector**(1/action_dim) for _ in range(action_dim)], axis=1))
+            self.surr2 = self.ratio_coordinate_wise * self.alpha_vector * advantages
 
-            self.surr2 = ratio_coordinate_wise * self.alpha_vector * advantages
             self.surr = tf.reduce_sum(tf.minimum(self.surr1, self.surr2), reduction_indices=[1])
 
         else:
